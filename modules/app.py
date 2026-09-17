27e94ae3cc4ebac0b91b1831e4f983be782b8917
Hrick <hritabratadas8@gmail.com>
2026-09-17 19:48:10 +0530

    allowed frontend cors

--- modules/app.py
+++ modules/app.py
@@ -29,7 +29,7 @@ def create_app(settings: Settings | None = None) -> FastAPI:
     application = FastAPI(title="SWEPilot", version="1.0.0")
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+        allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://swepilot.hrick.in/"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
